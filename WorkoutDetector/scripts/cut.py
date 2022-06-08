840b4a79d949f3007f3fe6963f0a422319391028
iucario <16816842+iucario@users.noreply.github.com>
2022-06-08 16:22:33 +0900

    Update dataset csv files Add col indicating rep start and end frame to Countix. Add YouTube id to Repcount. Add Repcount original csv.

--- WorkoutDetector/scripts/cut.py
+++ WorkoutDetector/scripts/cut.py
@@ -7,9 +7,9 @@ import yaml
 config = yaml.safe_load(open(os.path.join(os.path.dirname(__file__), '../utils/config.yml')))
 
 
-def cut_video(label, vid, start_sec, end_sec, output_folder):
-    input_file = os.path.join(config.proj_root, 'data', label, f'{vid}.mp4')
-    output_file = os.path.join(output_folder, f'{vid}_{start_sec}.mp4')
+def cut_video(input_dir, vid, start_sec, end_sec, output_dir):
+    input_file = os.path.join(input_dir, f'{vid}.mp4')
+    output_file = os.path.join(output_dir, f'{vid}_{start_sec}.mp4')
     cmd = f'ffmpeg -i {input_file} -ss {start_sec} -to {end_sec}'\
         f'  -filter:v fps=30 {output_file}'
     subprocess.call(cmd, shell=True)
