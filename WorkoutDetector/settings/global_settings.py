d37b20395923a8fb2861c07bfd01b310c2d87773
iucario <16816842+iucario@users.noreply.github.com>
2022-06-17 17:40:44 +0900

    Update global settings for Google Colab

--- WorkoutDetector/settings/global_settings.py
+++ WorkoutDetector/settings/global_settings.py
@@ -1,6 +1,6 @@
 import os
 
-PROJ_ROOT = 'your_project_root_here'
+PROJ_ROOT = '/content'
 
 _repcount_anno_relative_path = 'datasets/RepCount/annotation.csv'
 REPCOUNT_ANNO_PATH = os.path.join(PROJ_ROOT, _repcount_anno_relative_path)
