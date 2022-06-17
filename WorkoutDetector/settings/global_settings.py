a7fe56c606876c07c5db2813855030b6b4700adc
iucario <16816842+iucario@users.noreply.github.com>
2022-06-17 17:49:40 +0900

    Update PROJ_ROOT

--- WorkoutDetector/settings/global_settings.py
+++ WorkoutDetector/settings/global_settings.py
@@ -1,6 +1,6 @@
 import os
 
-PROJ_ROOT = '/content'
+PROJ_ROOT = '/content/WorkoutDetector/'
 
 _repcount_anno_relative_path = 'datasets/RepCount/annotation.csv'
 REPCOUNT_ANNO_PATH = os.path.join(PROJ_ROOT, _repcount_anno_relative_path)
