24d92d9eddf3b352edddd018151a1917ab268651
iucario <16816842+iucario@users.noreply.github.com>
2022-06-28 21:29:22 +0900

    Update script for building workout dataset

--- workoutdetector/scripts/build_datasets.py
+++ workoutdetector/scripts/build_datasets.py
@@ -1,15 +1,15 @@
+import argparse
 import os
-
+from os.path import join as osj
 import numpy as np
 import pandas as pd
-import torch
-import torchvision.transforms.functional as TF
 from PIL import Image
 from torchvision.io.video import read_video
 from workoutdetector.settings import PROJ_ROOT
+from subprocess import call
 
 
-def build_image_rep(data_dir: str, anno_path: str, dest_dir: str) -> None:
+def build_image_rep() -> None:
     """Creates images for torchvision.datasets.ImageFolder. Repetition states.
 
     Args:
@@ -17,7 +17,9 @@ def build_image_rep(data_dir: str, anno_path: str, dest_dir: str) -> None:
         anno_path: csv file path
         dest_dir: images will be saved in `dest_dir/train/category/image_1.png`.
     """
-
+    data_dir = osj(PROJ_ROOT, 'data/RepCount/video')
+    anno_path = osj(PROJ_ROOT, '/data/RepCount/annotation.csv')
+    dest_dir = osj(PROJ_ROOT, 'data/RepCount/rep_image')
     CLASSES = ['situp', 'push_up', 'pull_up', 'jump_jack', 'squat', 'front_raise']
     if not os.path.exists(dest_dir):
         os.makedirs(dest_dir)
@@ -59,8 +61,115 @@ def build_image_rep(data_dir: str, anno_path: str, dest_dir: str) -> None:
     print('Done')
 
 
+def build_workout():
+    """Creates the Workout dataset in `data/Workout/`.
+    Expects dataset Countix and RepCount to already in `data/`.
+    Merge the two datasets and relabel the classes.
+    9 classes from RepCount and 10 from Countix. To a new dataset Workout with 11 classes.
+    """
+
+    assert os.path.isdir(os.path.join(PROJ_ROOT, 'data/RepCount'))
+    assert os.path.isdir(os.path.join(PROJ_ROOT, 'data/Countix'))
+
+    classes = [
+        'front_raise', 'pull_up', 'squat', 'bench_pressing', 'jumping_jack', 'situp',
+        'push_up', 'battle_rope', 'exercising_arm', 'lunge', 'mountain_climber'
+    ]
+
+    repcount_class = []
+    countix_class = []
+
+    with open(os.path.join(PROJ_ROOT, 'datasets/RepCount/classes.txt')) as f:
+        for line in f:
+            repcount_class.append(line.strip())
+
+    with open(os.path.join(PROJ_ROOT, 'datasets/Countix/classes.txt')) as f:
+        for line in f:
+            countix_class.append(line.strip())
+
+    # map RepCount classes to Workout classes
+    repcount_to = [
+        'front_raise', 'pull_up', 'squat', 'bench_pressing', 'jumping_jack', 'situp',
+        'push_up', 'battle_rope'
+    ]
+    repcount_d = {}
+    for i, x in enumerate(repcount_class):
+        if x in repcount_to:
+            repcount_d[i] = classes.index(x)
+
+    # map Countix classes to Workout classes
+    countix_to = [
+        'exercising_arm', 'bench_pressing', 'front_raise', 'squat', 'jumping_jack',
+        'lunge', 'mountain_climber', 'pull_up', 'push_up', 'situp'
+    ]
+
+    countix_d = dict()
+    for i, x in enumerate(countix_class):
+        y = countix_to[i]
+        j = classes.index(y)
+        countix_d[i] = j
+
+    print(repcount_d)
+    print(countix_d)
+
+    # write the new labels to file
+
+    def build(label_map: dict, prefix: str, input_txt: str, output_txt: str) -> None:
+        set_type = input_txt.split('/')[-1].split('.')[0]
+        with open(input_txt, 'r') as f:
+            lines = f.readlines()
+        with open(output_txt, 'w') as f:
+            for line in lines:
+                path, length, label = line.rstrip().split()
+                path, length, label = line.split()
+                i = int(label)
+                if i in label_map:
+                    label = label_map[i]
+                else:
+                    continue
+                path = '/'.join([prefix, set_type, path])
+                f.write(f'{path} {length} {label}\n')
+
+    # new large dataset will be in path: data/Workout
+    build(repcount_d, 'RepCount',
+          os.path.join(PROJ_ROOT, 'data/RepCount/rawframes/train.txt'),
+          os.path.join(PROJ_ROOT, 'data/Workout/rawframes/train_repcount.txt'))
+    build(repcount_d, 'RepCount',
+          os.path.join(PROJ_ROOT, 'data/RepCount/rawframes/val.txt'),
+          os.path.join(PROJ_ROOT, 'data/Workout/rawframes/val_repcount.txt'))
+    build(repcount_d, 'RepCount',
+          os.path.join(PROJ_ROOT, 'data/RepCount/rawframes/test.txt'),
+          os.path.join(PROJ_ROOT, 'data/Workout/rawframes/test_repcount.txt'))
+    # Countix
+    build(countix_d, 'Countix', os.path.join(PROJ_ROOT,
+                                             'data/Countix/rawframes/train.txt'),
+          os.path.join(PROJ_ROOT, 'data/Workout/rawframes/train_countix.txt'))
+    build(countix_d, 'Countix', os.path.join(PROJ_ROOT, 'data/Countix/rawframes/val.txt'),
+          os.path.join(PROJ_ROOT, 'data/Workout/rawframes/val_countix.txt'))
+
+    # Merge files
+    # cat train_repcount.txt train_countix.txt > train.txt
+    # cat val_repcount.txt val_countix.txt > val.txt
+    # cat test_repcount.txt > test.txt
+
+    # link to Workouts
+    # ln -s data/RepCount/rawframes/train data/Workout/rawframes/RepCount/train
+    # ln -s data/RepCount/rawframes/val data/Workout/rawframes/RepCount/val
+    # ln -s data/RepCount/rawframes/test data/Workout/rawframes/RepCount/test
+    # ln -s data/Countix/rawframes/train data/Workout/rawframes/Countix/train
+    # ln -s data/Countix/rawframes/val data/Workout/rawframes/Countix/val
+
+
+def parse_args(argv=None) -> None:
+    parser = argparse.ArgumentParser()
+    parser.add_argument('func',
+                        type=str,
+                        help='function to run',
+                        choices=['build_workout', 'build_video'])
+    args = parser.parse_args(argv)
+    func = globals()[args.func]
+    func()
+
+
 if __name__ == '__main__':
-    data_dir = os.path.expanduser('~/data/RepCount/video')
-    anno_path = os.path.expanduser('~/data/RepCount/annotation.csv')
-    dest_dir = os.path.expanduser('~/data/RepCount/rep_image')
-    build_image_rep(data_dir, anno_path, dest_dir)
+    parse_args()
--- workoutdetector/scripts/download.py
+++ workoutdetector/scripts/download.py
@@ -1,8 +1,26 @@
+import base64
 import os
 from pathlib import Path
+
 import pandas as pd
 import yt_dlp
 from workoutdetector.settings import PROJ_ROOT
+
+
+def parse_onedrive(link: str) -> str:
+    """Parse onedrive link to download link.
+
+    Args:
+        link: str, start with `https://1drv.ms/u/s!`
+
+    Returns:
+        str, download link.
+    """
+    assert link.startswith('https://1drv.ms/u/s!')
+    b = base64.urlsafe_b64encode(link.strip().encode('ascii'))
+    s = b.decode('ascii')
+    res = f'https://api.onedrive.com/v1.0/shares/u!{s}/root/content'
+    return res
 
 
 def download_ytb(url, folder='~'):
@@ -32,6 +50,15 @@ def download_repcount(csv_path, folder='~'):
         download_ytb(url, folder)
 
 
+def _get_url():
+    URL_VIDEO = parse_onedrive('https://1drv.ms/u/s!AiohV3HRf-34ipk0i1y2P1txpKYXFw')
+    URL_ANNO = parse_onedrive('https://1drv.ms/u/s!AiohV3HRf-34i_YvMob5Vpgvxjc3mQ')
+    URL_RAWFRAME = parse_onedrive('https://1drv.ms/u/s!AiohV3HRf-34ipwACYfKSHhkZzebrQ')
+    COUNTIX = parse_onedrive('https://1drv.ms/u/s!AiohV3HRf-34ipwWwzztzGynyj5Fwg')
+    print(COUNTIX)
+
+
 if __name__ == '__main__':
-    download_repcount(Path(PROJ_ROOT, 'datasets/RepCount/all_data.csv'),
-                      '/mnt/d/repcount-redown')
+    # download_repcount(Path(PROJ_ROOT, 'datasets/RepCount/all_data.csv'),
+    #                   '/mnt/d/repcount-redown')
+    _get_url()
